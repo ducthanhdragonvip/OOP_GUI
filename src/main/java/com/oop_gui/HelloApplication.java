5492c4f628e9dc5b19fbeef7da0cc0619b16f283
thanh <ducthanhdragonvip@gmail.com>
2023-12-06 23:55:25 +0700

    add css

--- src/main/java/com/oop_gui/HelloApplication.java
+++ src/main/java/com/oop_gui/HelloApplication.java
@@ -11,7 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainApp.fxml"));
+        String css = this.getClass().getResource("/style/style.css").toExternalForm();
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        scene.getStylesheets().add(css);
         stage.setTitle("Chat_Box!");
         stage.setScene(scene);
         stage.show();
